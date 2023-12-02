a603050f4dcea93c182db5f2579b98d85d579bb1
Hamza Mahmood <hamza.mahmood@outlook.fr>
2023-12-02 10:34:42 +0100

    DEV-33: template sidebar header

---
+++ src/Components/Alert.php
@@ -0,0 +1,13 @@
+<?php
+
+// src/Twig/Components/Alert.php
+namespace App\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+class Alert
+{
+    public string $type = 'success';
+    public string $message;
+}
---
+++ src/Components/Header.php
@@ -0,0 +1,12 @@
+<?php
+
+// src/Twig/Components/Alert.php
+namespace App\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+class Header
+{
+
+}
