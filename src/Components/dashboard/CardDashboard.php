2b4519d0b997e0ebcc67d07c31010e94fec76b85
Hamza Mahmood <hamza.mahmood@outlook.fr>
2024-02-23 17:42:50 +0100

    feature/DEV-58: responsive

--- src/Components/dashboard/CardDashboard.php
+++ src/Components/dashboard/CardDashboard.php
@@ -7,7 +7,7 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent(template: 'components/dashboard/card_dashboard.html.twig')]
 class CardDashboard
 {
-    public string $src;
+    public string $picto;
     public string $number;
     public string $title;
 }
